c1fa5b5f3abc907f07c0742b12e65d9079422cfd
delletran <delio.letran@gmail.com>
2023-06-23 11:39:06 +0800

    initiate workflow

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
 export const API_URL='http://127.0.0.1:8090'
-export const FE_URL='http://127.0.0.1:3000'
+export const FE_URL='https://main.d1vv1a9huujpiz.amplifyapp.com/'
 export const baseUrl = API_URL
 export const publicUrl = FE_URL
